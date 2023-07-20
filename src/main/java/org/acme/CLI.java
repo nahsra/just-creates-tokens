814cb00c1accd199bc012d370ee9fdc6a73e3e60
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-07-20 10:41:38 -0400

    update CLI

---
+++ src/main/java/org/acme/CLI.java
@@ -0,0 +1,19 @@
+package org.acme;
+
+import java.util.Random;
+
+public class CLI {
+
+  /* people call this and look for the new token on stdout */
+  public static void main(String[] args) {
+    System.out.println(createToken());
+  }
+ 
+  private String createToken() {
+    Random rnd = new Random();
+    int number = rnd.nextInt(999999);
+    // this will convert the random number sequence into 6 character
+    return String.format("%06d", number);
+  }
+ 
+}
